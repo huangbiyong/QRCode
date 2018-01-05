469e478fd8ee646c09369fefe54807c13b7084cb
huangbiyong <huangbiyong@diandian-tech.com>
2018-01-05 10:22:26 +0800

    Initial Commit

---
+++ 二维码综合/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  二维码综合
+//
+//  Created by huangbiyong on 2018/1/5.
+//  Copyright © 2018年 com.chase. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
